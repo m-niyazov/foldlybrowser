7559da54a439f907edadd15150b2a6321467f331
m-niyazov <niyazov.developer@gmail.com>
2025-06-15 20:34:18 +0300

    Initial project

---
+++ foldlybrowser/foldlybrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foldlybrowser
+//
+//  Created by TapticGroup on 15/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
