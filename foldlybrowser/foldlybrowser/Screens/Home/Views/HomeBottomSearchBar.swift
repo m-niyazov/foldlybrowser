34192ad4bd0452c9be497570d755a4e4b79872a3
m-niyazov <niyazov.developer@gmail.com>
2025-07-31 00:48:36 +0300

    major fixes

--- foldlybrowser/foldlybrowser/Screens/Home/Views/HomeBottomSearchBar.swift
+++ foldlybrowser/foldlybrowser/Screens/Home/Views/HomeBottomSearchBar.swift
@@ -22,11 +22,16 @@ final class HomeBottomSearchBar: UIView {
     private var widthEqualConstraint: Constraint!
     let searchTextField = UITextField()
     private let searchEngineIcon = UIImageView()
+    private let refreshButton = UIButton(type: .system)
+
+    private let bottomContainer = UIView()
+    private let backButton = UIButton(type: .system)
+    private let forwardButton = UIButton(type: .system)
+    private let leadingButtonsStackView = UIStackView()
 
     private let homeButton = UIButton(type: .system)
-    private let backButton = UIButton(type: .system)
+    private let saveButton = UIButton(type: .system)
     private let menuButton = UIButton(type: .system)
-    private let closeButton = UIButton(type: .system)
 
     // MARK: – Public callbacks
     var didTapHome: (() -> Void)?
@@ -53,9 +58,9 @@ final class HomeBottomSearchBar: UIView {
     func makeSearchBarActive(keyboardHeight: CGFloat) {
         UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseIn]) {
             self.bluryBackgroundHeightConstraint.update(offset: keyboardHeight + 45 + 20)
-            self.searchTextFieldContainerWidthLessConstraint.update(priority: .low)
-            self.searchTextFieldContainerWidthEqualConstraint.activate()
-            self.searchTextFieldContainerWidthEqualConstraint.update(priority: .high)
+            // self.searchTextFieldContainerWidthLessConstraint.update(priority: .low)
+            // self.searchTextFieldContainerWidthEqualConstraint.activate()
+            // self.searchTextFieldContainerWidthEqualConstraint.update(priority: .high)
             self.superview?.layoutIfNeeded()
         }
     }
@@ -63,8 +68,8 @@ final class HomeBottomSearchBar: UIView {
     func makeSearchBarInActive() {
         UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut]) {
             self.bluryBackgroundHeightConstraint.update(offset: 0)
-            self.searchTextFieldContainerWidthLessConstraint.update(priority: .high)
-            self.searchTextFieldContainerWidthEqualConstraint.deactivate()
+            // self.searchTextFieldContainerWidthLessConstraint.update(priority: .high)
+            // self.searchTextFieldContainerWidthEqualConstraint.deactivate()
             self.superview?.layoutIfNeeded()
         }
     }
@@ -86,21 +91,31 @@ private extension HomeBottomSearchBar {
         }
 
         searchTextFieldContainer.do {
-            $0.backgroundColor = .systemGray6
+            $0.backgroundColor = .white
             $0.axis = .horizontal
             $0.distribution = .fill
-            $0.spacing = 5
             $0.alignment = .center
-            $0.layer.cornerRadius = 22.5
+            $0.layer.cornerRadius = 25
             $0.layer.borderColor = UIColor.systemGray5.cgColor
             $0.layer.borderWidth = 1
-            $0.layoutMargins = UIEdgeInsets(top: 0, left: 7, bottom: 0, right: 0)
+            $0.layoutMargins = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
             $0.isLayoutMarginsRelativeArrangement = true
         }
 
         searchEngineIcon.do {
             $0.contentMode = .scaleAspectFit
             $0.image = .googleIcon
+        }
+
+        refreshButton.do {
+            $0.setImage(
+                .init(
+                    systemName: "arrow.clockwise",
+                    withConfiguration: UIImage.SymbolConfiguration(pointSize: 14, weight: .semibold)
+                ),
+                for: .normal
+            )
+            $0.tintColor = .black
         }
 
         searchTextField.do {
@@ -110,48 +125,122 @@ private extension HomeBottomSearchBar {
             $0.rightView = rightView
             $0.rightViewMode = .unlessEditing
             $0.leftViewMode = .always
-            $0.backgroundColor = .systemGray5
+            $0.backgroundColor = .none
             $0.clearButtonMode = .whileEditing
             $0.placeholder = "Search or enter website"
+//            $0.attributedPlaceholder = NSAttributedString(
+//                string: "Search or enter website",
+//                attributes: [NSAttributedString.Key.foregroundColor : UIColor.systemGray]
+//            )
             $0.borderStyle = .none
             $0.clearButtonMode = .whileEditing
-            $0.layer.cornerRadius = 22.5
+            $0.layer.cornerRadius = 0
             $0.font = .preferredFont(forTextStyle: .body)
             $0.returnKeyType = .go
             $0.delegate = self
         }
 
+        leadingButtonsStackView.do {
+            $0.spacing = 10
+            $0.axis = .horizontal
+        }
 
-        homeButton.do {
-            $0.setImage(
-                UIImage(systemName: "house", withConfiguration: UIImage.SymbolConfiguration(pointSize: 14, weight: .bold)),
-                for: .normal
-            )
-            $0.addTarget(self, action: #selector(tapHome), for: .touchUpInside)
+        [backButton, menuButton, forwardButton].forEach {
+            $0.layer.cornerRadius = 17.5
+            $0.tintColor = .black
+            $0.layer.borderColor = UIColor.systemGray5.cgColor
+            $0.layer.borderWidth = 1
+            $0.backgroundColor = .systemGray5.withAlphaComponent(0.6)
         }
 
         backButton.do {
-            $0.setImage(.init(systemName: "chevron.backward"), for: .normal)
+            $0.setImage(
+                .init(
+                    systemName: "chevron.backward",
+                    withConfiguration: UIImage.SymbolConfiguration(pointSize: 14, weight: .medium)
+                ),
+                for: .normal
+            )
+        }
+
+        forwardButton.do {
+            $0.setImage(
+                .init(
+                    systemName: "chevron.forward",
+                    withConfiguration: UIImage.SymbolConfiguration(pointSize: 14, weight: .medium)
+                ),
+                for: .normal
+            )
         }
 
         menuButton.do {
-            $0.setImage(.init(systemName: "ellipsis"), for: .normal)
+            $0.setImage(
+                .init(
+                    systemName: "ellipsis",
+                    withConfiguration: UIImage.SymbolConfiguration(pointSize: 14, weight: .medium)
+                ),
+                for: .normal
+            )
         }
-        
-        [homeButton, backButton, menuButton, closeButton].forEach {
-            $0.layer.cornerRadius = 17.5
+
+
+        homeButton.do {
+            $0.setImage(.cmHomeIcon.withRenderingMode(.alwaysOriginal), for: .normal)
+            var configuration = UIButton.Configuration.plain()
+            configuration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 12, bottom: 10, trailing: 12)
+            configuration.imagePadding = 8
+            configuration.attributedTitle = .init("Back To Home", attributes: .init([
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .semibold),
+                NSAttributedString.Key.foregroundColor: UIColor.accent.cgColor,
+            ]))
+            $0.configuration = configuration
+            $0.backgroundColor = .accent.withAlphaComponent(0.08)
+            $0.tintColor = .accent
+            $0.layer.cornerRadius = 20
+            $0.layer.masksToBounds = true
+            $0.addTarget(self, action: #selector(tapHome), for: .touchUpInside)
+        }
+
+
+        saveButton.do {
+            $0.setImage(
+                .init(
+                    systemName: "plus",
+                    withConfiguration: UIImage.SymbolConfiguration(pointSize:11, weight: .bold)
+                ),
+                for: .normal
+            )
+            var configuration = UIButton.Configuration.plain()
+            configuration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 12, bottom: 10, trailing: 12)
+            configuration.imagePadding = 5
+            configuration.attributedTitle = .init("Save", attributes: .init([
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .semibold),
+                NSAttributedString.Key.foregroundColor: UIColor.black.cgColor,
+            ]))
+            $0.configuration = configuration
+            $0.backgroundColor = UIColor.systemGray5
+            $0.layer.borderColor = UIColor.systemGray5.cgColor
+            $0.layer.borderWidth = 1
             $0.tintColor = .black
-            $0.backgroundColor = .systemGray5
+            $0.layer.cornerRadius = 20
+            $0.layer.masksToBounds = true
         }
+
     }
 
     func addSubviews() {
         addSubview(bluryBackground)
         bluryBackground.contentView.addSubview(contentView)
         contentView.addSubview(searchTextFieldContainer)
-        contentView.addSubview(homeButton)
         searchTextFieldContainer.addArrangedSubview(searchEngineIcon)
         searchTextFieldContainer.addArrangedSubview(searchTextField)
+        searchTextFieldContainer.addArrangedSubview(refreshButton)
+        contentView.addSubview(bottomContainer)
+        bottomContainer.addSubview(leadingButtonsStackView)
+        leadingButtonsStackView.addArrangedSubview(backButton)
+        leadingButtonsStackView.addArrangedSubview(forwardButton)
+        bottomContainer.addSubview(homeButton)
+        bottomContainer.addSubview(saveButton)
 
         bluryBackground.snp.makeConstraints {
             $0.top.equalToSuperview()
@@ -166,25 +255,51 @@ private extension HomeBottomSearchBar {
             $0.bottom.lessThanOrEqualTo(safeAreaLayoutGuide)
         }
 
-        homeButton.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.size.equalTo(35)
-            $0.trailing.equalTo(searchTextFieldContainer.snp.leading).offset(-16)
+        searchEngineIcon.snp.makeConstraints {
+            $0.size.equalTo(25)
         }
 
         searchTextFieldContainer.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            self.searchTextFieldContainerWidthLessConstraint = $0.width.lessThanOrEqualToSuperview().priority(.high).constraint
-            self.searchTextFieldContainerWidthEqualConstraint = $0.width.equalToSuperview().priority(.low).constraint
+            $0.top.equalToSuperview()
+            $0.width.equalToSuperview()
+            // self.searchTextFieldContainerWidthLessConstraint = $0.width.lessThanOrEqualToSuperview().priority(.high).constraint
+            // self.searchTextFieldContainerWidthEqualConstraint = $0.width.equalToSuperview().priority(.low).constraint
             $0.centerX.equalToSuperview()
         }
 
-        searchEngineIcon.snp.makeConstraints {
-            $0.size.equalTo(22.5)
+        searchTextField.snp.makeConstraints {
+            $0.height.equalTo(50)
         }
 
-        searchTextField.snp.makeConstraints {
-            $0.height.equalTo(45)
+        refreshButton.snp.makeConstraints {
+            $0.size.equalTo(30)
+        }
+
+        bottomContainer.snp.makeConstraints {
+            $0.top.equalTo(searchTextFieldContainer.snp.bottom).offset(10)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+
+        homeButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+
+        leadingButtonsStackView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.centerY.equalTo(homeButton)
+        }
+
+        saveButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(homeButton)
+            $0.centerY.equalTo(homeButton)
+        }
+
+        [backButton, forwardButton].forEach {
+            $0.snp.makeConstraints {
+                $0.size.equalTo(35)
+            }
         }
     }
 
